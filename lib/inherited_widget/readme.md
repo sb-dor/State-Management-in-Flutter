c76ba6cbf311e23497cd84cfdaa1ca9a93cc493a
sb-dor <avaz.shams.2002@gmail.com>
2024-10-13 20:06:19 +0500

    added comments

--- lib/inherited_widget/readme.md
+++ lib/inherited_widget/readme.md
@@ -4,3 +4,4 @@ https://api.flutter.dev/flutter/widgets/InheritedNotifier-class.html
 
 1. https://youtu.be/n_HLJUBkc48?t=2265
 2. https://www.youtube.com/watch?v=WmgUuDojbm4
+3. https://stackoverflow.com/questions/66882776/pass-data-to-multiple-screens-with-inheritedwidget
--- lib/inherited_widget/todo_with_inherited_notifier/main_td_inhnot.dart
+++ lib/inherited_widget/todo_with_inherited_notifier/main_td_inhnot.dart
@@ -5,6 +5,9 @@ import 'view_model/td_inh_view_model.dart';
 
 // it's not a good idea
 // better use provider package
+
+// info about wrapping inherits above material app:
+// https://stackoverflow.com/questions/66882776/pass-data-to-multiple-screens-with-inheritedwidget
 class MainTdInhNot extends StatefulWidget {
   const MainTdInhNot({super.key});
 
