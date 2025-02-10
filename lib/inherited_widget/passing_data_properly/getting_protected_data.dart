fad8b773e05cc2511cda895ef8f8fb4f608a5b1b
sb-dor <avaz.shams.2002@gmail.com>
2025-02-10 22:51:09 +0500

    added more information about @protected fields and methods

---
+++ lib/inherited_widget/passing_data_properly/getting_protected_data.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:state_management_course/inherited_widget/passing_data_properly/passing_data_widget.dart';
+
+// just imagine that this class is one the child of "PassingDataWidget"
+// when you want to get "surnameController" from PassingDataWidgetState
+// it will not allow you, because it's "@protected"
+class GettingProtectedData extends StatefulWidget {
+  const GettingProtectedData({super.key});
+
+  @override
+  State<GettingProtectedData> createState() => _GettingProtectedDataState();
+}
+
+class _GettingProtectedDataState extends State<GettingProtectedData> {
+
+  late final TextEditingController surnameController;
+
+  @override
+  void initState() {
+    super.initState();
+    surnameController = PassingDataWidget.state(context).; // not visible
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
