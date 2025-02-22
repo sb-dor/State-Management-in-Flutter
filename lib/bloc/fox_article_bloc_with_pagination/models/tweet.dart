55d43fe7a37078f511c7c363a57e0571c5533fc4
sarbador <avaz.shams.2002@gmail.com>
2025-02-22 11:05:19 +0500

    fox article bloc 4 - created models

---
+++ lib/bloc/fox_article_bloc_with_pagination/models/tweet.dart
@@ -0,0 +1,25 @@
+typedef TweetId = String;
+
+// create abstract class
+// think that there is a class Tweet which was created
+// this is just a draft
+abstract class Tweet implements Comparable<Tweet> {
+  abstract final TweetId tweetId;
+
+  factory Tweet.fromJson(Map<String, Object?> json) => throw UnimplementedError();
+
+  Map<String, Object?> toJson();
+
+  @override
+  int compareTo(Tweet other) {
+    return tweetId.compareTo(other.tweetId);
+  }
+}
+
+// class that state will be inside states
+// for handling list, bool
+abstract class TweetChunk {
+  abstract final List<Tweet> tweets;
+  abstract final String? cursor;
+  abstract final bool hasMore;
+}
